07b86947f003c47b04ae741184f3dca61ef582d8
Julio <bevilacquajulio@outlook.com>
2025-04-01 09:54:31 -0300

    Botão dujão arquivos adicionados

---
+++ botao_fujao/script.js
@@ -0,0 +1,22 @@
+function parabens() {
+  alert("Parabéns, você tem muito bom gosto!");
+}
+
+var botaoNao = document.getElementById("nao");
+var tela = document.getElementById("tela");
+
+function fuja() {
+  
+  var alturaJanela = tela.offsetHeight
+  var larguraJanela = tela.offsetWidth;
+
+  var maxX = larguraJanela - botaoNao.offsetWidth;
+  var maxY = alturaJanela - botaoNao.offsetHeight;
+  
+  var aleatorioX = Math.floor(Math.random()*maxX);
+  var aleatorioY = Math.floor(Math.random()*maxY);
+  
+  
+  botaoNao.style.left = aleatorioX + "px";
+  botaoNao.style.top = aleatorioY + "px";
+}
